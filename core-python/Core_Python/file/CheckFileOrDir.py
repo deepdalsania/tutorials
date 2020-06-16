fed9b1f238995a67955b4a483a7bb459903d835c
deepdalsania <deeppatel.dd@gmail.com>
2020-06-16 19:14:59 +0530

    add comment

--- core-python/Core_Python/file/CheckFileOrDir.py
+++ core-python/Core_Python/file/CheckFileOrDir.py
@@ -4,5 +4,6 @@ if __name__ == "__main__":
     dir = "E:/GitHub/1) Git_Tutorials_Repo_Projects/core-python/Core_Python/ExCheckDir";
     for name in os.listdir(dir):
         fullname = os.path.join(dir, name)
+        # if you want to check file then use isfile()
         print("{} is a directory,".format(fullname)) if os.path.isdir(fullname) else print(
             "{} is a file.".format(fullname))
