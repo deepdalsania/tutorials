99ae7cfd2201538af9e7b7f4f79f94b0f636e062
deepdalsania <deeppatel.dd@gmail.com>
2020-06-23 21:16:08 +0530

    bug solved

--- python-pandas/Python_Pandas/dataframe/CreateDfFromListofDict.py
+++ python-pandas/Python_Pandas/dataframe/CreateDfFromListofDict.py
@@ -3,11 +3,11 @@ import pandas as pd
 # The dictionary keys are by default taken as column names.
 d = [{'a': 1, 'b': 2}, {'a': 4.10, 'b': 2.25, 'c':6}]
 df = pd.DataFrame(data=d)
-print("DataFrame from LList of Dict : ")
+print("DataFrame from List of Dict : ")
 print(df)
 df = pd.DataFrame(data=d,index=['first','second'])
-print("DataFrame from LList of Dict with index : ")
+print("DataFrame from List of Dict with index : ")
 print(df)
 df = pd.DataFrame(data=d,index=['first','second'],columns=['a','c'])
-print("DataFrame from LList of Dict with index and columns : ")
+print("DataFrame from List of Dict with index and columns : ")
 print(df)
